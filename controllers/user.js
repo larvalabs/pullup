74711f6f247f9e0d50b26d38731df26f438e3b90
sp1d3rx <sp1d3rx@gmail.com>
2014-02-06 16:35:56 -0700

    Update user.js
    removed 'gender'

--- controllers/user.js
+++ controllers/user.js
@@ -119,7 +119,6 @@ exports.postUpdateProfile = function(req, res, next) {
     if (err) return next(err);
     user.email = req.body.email || '';
     user.profile.name = req.body.name || '';
-    user.profile.gender = req.body.gender || '';
     user.profile.location = req.body.location || '';
     user.profile.website = req.body.website || '';
 
