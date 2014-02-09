60b13c869cfaf5d9767ed626f0c18270cb95874b
Neal Lambert <crankeye@gmail.com>
2014-02-09 02:53:34 -0700

    Fixing mistake: Should append not overwrite contents of the /etc/ansible/hosts file.

--- vagrant/ansible-windows.sh
+++ vagrant/ansible-windows.sh
@@ -2,7 +2,7 @@ echo "Installing/Updating Ansible..."
 apt-get update -qq && apt-get install ansible -y -qq
 COUNT_HOSTS=`grep \\\[precise64\\\] /etc/ansible/hosts -c`
 if [ $COUNT_HOSTS -lt 1 ]; then
-	printf "\n[precise64] \n127.0.0.1 ansible_connection=local\n" > /etc/ansible/hosts
+	printf "\n[precise64] \n127.0.0.1 ansible_connection=local\n" >> /etc/ansible/hosts
 fi
 echo "Running Ansible playbook..."
 ansible-playbook /vagrant/vagrant/playbook.yml
