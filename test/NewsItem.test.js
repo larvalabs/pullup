ff22c6f91c4450e1708d5f78928addaf093d0695
Carl Groner <c.groner@gmail.com>
2014-02-12 19:17:54 -0800

    Correct self-post identification.

---
+++ test/NewsItem.test.js
@@ -0,0 +1,36 @@
+describe('NewsItem', function(){
+
+  var should = require('should'),
+      NewsItem = require('../models/NewsItem'),
+      item = {};
+
+  beforeEach(function(done) {
+    item = new NewsItem();
+    done();
+  });
+
+  describe('sanity test', function() {
+    it('Item should exist and have an ID', function(){
+      item.should.be.ok;
+      item._id.should.be.ok;
+    })
+  });
+
+  describe('#isSelfPost()', function(){
+    it('should be true when the URL points to this post', function(){
+      item.url = '/news/' + item._id;
+      item.isSelfPost().should.be.true;
+    });
+
+    it('should NOT be true if the URL does not point to this post', function(){
+      item.url = 'http://pullup.io/about';
+      item.isSelfPost().should.not.be.true;
+    });
+
+    it('should NOT be true just because the source is "pullup.io"', function(){
+      item.source = 'pullup.io';
+      item.isSelfPost().should.not.be.true;
+    });
+  })
+
+})
