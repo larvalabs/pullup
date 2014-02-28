8e75b497fcbd0e7ee73ec62222fc59d6881160d9
Joe Wegner <joe@wegnerdesign.com>
2014-02-28 14:39:45 -0600

    flag the footer piece, and log the releases body

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ if(secrets.heroku.email && secrets.heroku.authToken && secrets.heroku.app) {
   console.log(secrets.heroku);
   request.get('https://api.heroku.com/apps/'+encodeURIComponent(secrets.heroku.app)+'/releases', {
     auth: {
-      user: secrets.heroku.email, 
+      user: secrets.heroku.email,
       pass: secrets.heroku.authToken
     },
     headers: {
@@ -66,6 +66,7 @@ if(secrets.heroku.email && secrets.heroku.authToken && secrets.heroku.app) {
     }
   },
   function(error, response, body) {
+    console.log("Heroku Responses:", body);
     if(!error) {
       var releases;
       try {
