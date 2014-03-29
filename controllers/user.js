6a0443881a7689546bd287a914baff9094211bcb
Brandon Clements <bc110663@scots.edinboro.edu>
2014-03-29 02:41:55 -0400

    Removed gender from user profile. Added negitivezero to user list.

--- controllers/user.js
+++ controllers/user.js
@@ -37,7 +37,6 @@ exports.postUpdateProfile = function(req, res, next) {
     if (err) return next(err);
     user.email = req.body.email || '';
     user.profile.name = req.body.name || '';
-    user.profile.gender = req.body.gender || '';
     user.profile.location = req.body.location || '';
     if (req.body.website.match(/https?:\/\//i)) {
       user.profile.website = req.body.website;
