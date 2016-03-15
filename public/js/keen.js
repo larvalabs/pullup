bb9ab5767aafca833ed73f738c392643de70affd
Rick Hanlon II <rickhanlonii@gmail.com>
2016-03-15 17:11:53 -0400

    Add /analytics

--- public/js/keen.js
+++ public/js/keen.js
@@ -1,24 +1,76 @@
-$(function() {
-  if (window.location.hostname === 'localhost') {
-    return;
-  }
+function reportPageView(client) {
+  if (window.location.hostname !== 'localhost') {
+    var pageViewData = {
+      ip_address: "${keen.ip}",
+      user_agent: "${keen.user_agent}",
+      referrer: document.referrer,
+      page: window.location.href
+    };
 
-  var keen = new Keen({
+    client.addEvent("pageviews", pageViewData, function (err, res) {
+      if (err) {
+        console.log("Keen.io error", err);
+      }
+    });
+  }
+}
+
+function renderPageViews(client) {
+  Keen.ready(function(){
+    var referrerQuery = new Keen.Query("count", {
+      eventCollection: "pageviews",
+      filters: [
+        {
+          "operator": "not_contains",
+          "property_name": "page",
+          "property_value": "localhost"
+        }
+      ],
+      groupBy: [
+        "referrer"
+      ],
+      timeframe: "this_14_days",
+      timezone: "UTC"
+    });
+
+    client.draw(referrerQuery, document.getElementById("keen-chart-referrer"), {
+      title: "Referrer"
+    });
+  });
+}
+
+function renderReferrals(client) {
+  Keen.ready(function() {
+    var query = new Keen.Query("count", {
+      eventCollection: "pageviews",
+      filters: [
+        {
+          "operator": "not_contains",
+          "property_name": "page",
+          "property_value": "localhost"
+        }
+      ],
+      groupBy: [
+        "page"
+      ],
+      timeframe: "this_14_days",
+      timezone: "UTC"
+    });
+
+    client.draw(query, document.getElementById("keen-chart-page"), {
+      title: "Pages"
+    });
+  });
+}
+
+$(function() {
+  var client = new Keen({
     projectId: pullup.keenProjectID,
     writeKey: pullup.keenWriteKey,
     readKey: pullup.keenReadKey
   });
 
-  var pageViewData = {
-    ip_address: "${keen.ip}",
-    user_agent: "${keen.user_agent}",
-    referrer: document.referrer,
-    page: window.location.href
-  };
-
-  keen.addEvent("pageviews", pageViewData, function (err, res) {
-    if (err) {
-      console.log("Keen.io error", err);
-    }
-  });
+  reportPageView(client);
+  renderPageViews(client);
+  renderReferrals(client);
 });
